3a9b52d5ae983742cc42ad0b12ec55b04e6a8302
Xed89 <simone.bosc@gmail.com>
2016-05-07 00:57:59 +0200

    Added modules

---
+++ UserInterface.cs
@@ -0,0 +1,44 @@
+﻿using bitLab.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loork_gui
+{
+  public enum Functionality
+  {
+    TriggerLevel,
+    VoltsPerDivision,
+    MicrosecondsPerDivision,
+  }
+
+  class UserInterface: CBaseVM
+  {
+    public UserInterface()
+    {
+      SetTriggerLevelFunctionality = new CDelegateCommand(mSetTriggerLevelFunctionality);
+      SetMicrosecondsPerDivisionFunctionality = new CDelegateCommand(mSetMicrosecondsPerDivisionFunctionality);
+    }
+
+    private Functionality mCurrentFunctionality;
+    public Functionality CurrentFunctionality
+    {
+      get { return mCurrentFunctionality; }
+      set { SetAndNotify(ref mCurrentFunctionality, value); }
+    }
+
+    public CDelegateCommand SetTriggerLevelFunctionality { get; private set; }
+    private void mSetTriggerLevelFunctionality(object arg)
+    {
+      CurrentFunctionality = Functionality.TriggerLevel;
+    }
+
+    public CDelegateCommand SetMicrosecondsPerDivisionFunctionality { get; private set; }
+    private void mSetMicrosecondsPerDivisionFunctionality(object arg)
+    {
+      CurrentFunctionality = Functionality.MicrosecondsPerDivision;
+    }
+  }
+}
